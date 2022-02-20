075df278562ff236b4b1fa732505eccdca35f01c
Berke Algül <47500887+berkealgul@users.noreply.github.com>
2022-02-20 21:55:03 +0300

    Add files via upload

--- DummyBlockchain/main.cpp
+++ DummyBlockchain/main.cpp
@@ -6,10 +6,8 @@ int main()
 	BlockChain someShitCoin;
 
 	someShitCoin.addBlock(new Block(0, "lmafo"));
-	someShitCoin.addBlock(new Block(0, "lmafoasd"));
-	someShitCoin.addBlock(new Block(0, "lmasadsadfo"));
-
-
+	someShitCoin.addBlock(new Block(1, "lmafoasd"));
+	someShitCoin.addBlock(new Block(2, "lmasadsadfo"));
 
 	return 0;
 }
